31563434ca5592e552e2cb6f71c4fc99aa04926d
趙子賢 <danny900714@gmail.com>
2022-11-08 02:50:17 +0800

    Add netlify edge function support (#1)
    * Add pocketbase and get auth methods
    
    * Add netlify edge function support

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
+<script>
+    /** @type {import('./$types').PageData} */
+    export let data;
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h3>Auth methods</h3>
+<ul>
+    {#if data.usernamePassword}
+        <li>Username/Password</li>
+    {/if}
+    {#if data.emailPassword}
+        <li>Email/Password</li>
+    {/if}
+</ul>
