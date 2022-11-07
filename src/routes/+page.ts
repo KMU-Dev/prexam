aed45df3f3a192e87c32c77dffa1688908e6ed61
趙子賢 <danny900714@gmail.com>
2022-11-08 02:30:39 +0800

    Add pocketbase and get auth methods

---
+++ src/routes/+page.ts
@@ -0,0 +1,7 @@
+import Pocketbase from 'pocketbase';
+
+/** @type {import('./$types').PageLoad} */
+export function load() {
+    const client = new Pocketbase('https://prexam.pockethost.io');
+    return client.collection('users').listAuthMethods();
+}
